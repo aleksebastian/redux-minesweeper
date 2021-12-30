844d013abd94c0ec2fc6588b30d05860a821e9f9
Alek <aleksebastian@gmail.com>
2021-12-30 16:54:35 -0600

    Added event handlers and extracted redux state to style and supply adjacent mine number to component

--- client/src/components/Cell.tsx
+++ client/src/components/Cell.tsx
@@ -1,11 +1,54 @@
 import React from "react";
-
 import styles from "./cell.module.css";
 
-const Cell = () => {
-  const handleClick = () => {};
+import { useAppSelector, useAppDispatch } from "../hooks";
+import { revealCell, markCell, gameOver, selectCells } from "./cellSlice";
 
-  return <div className={styles.cell}></div>;
+type CellProps = {
+  x: number;
+  y: number;
+  mine: boolean;
+  state: string;
+  number: number;
+};
+
+const Cell = ({ x, y }: CellProps) => {
+  const dispatch = useAppDispatch();
+  const reduxCellState = useAppSelector(selectCells);
+  const { mine, state, number } = reduxCellState[x][y];
+  const globalCellProps = { x, y, mine, state };
+
+  const handleLeftClick = (e: any) => {
+    e.preventDefault();
+    dispatch(markCell(globalCellProps));
+  };
+
+  const handleRightClick = (e: any) => {
+    e.preventDefault();
+    if (mine) {
+      dispatch(gameOver());
+    } else {
+      dispatch(revealCell(globalCellProps));
+    }
+  };
+
+  const cellStyles: any = {
+    hidden: { backgroundColor: "rgb(87 83 78)", cursor: "pointer" },
+    marked: { backgroundColor: "rgb(251 191 36)" },
+    number: { backgroundColor: "rgb(30 41 59)" },
+    mine: { backgroundColor: "rgb(220 38 38)" },
+  };
+
+  return (
+    <div
+      className={styles.cell}
+      style={cellStyles[state]}
+      onClick={handleRightClick}
+      onContextMenu={handleLeftClick}
+    >
+      {number && !mine && state === "number" ? number : null}
+    </div>
+  );
 };
 
 export default Cell;
