45d69e47fcf319f2086f2aaf354622e90b2b616f
Alek <aleksebastian@gmail.com>
2021-12-31 00:38:00 -0600

    Updated styles and added setStats action dispatch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import styles from "./app.module.css";
 import { Provider } from "react-redux";
 import store from "./store";
 
+import GameState from "./components/GameState";
 import Board from "./components/Board";
 
 const App = () => (
   <Provider store={store}>
     <div className={styles.main}>
-      <h1>Minesweeper</h1>
+      <h1 className={styles.header}>Minesweeper</h1>
+      <GameState />
       <Board />
     </div>
   </Provider>
--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -2,20 +2,22 @@ import React, { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import styles from "./board.module.css";
 
-import { useAppDispatch } from "../hooks";
+import { useAppDispatch, useAppSelector } from "../hooks";
 import { setBoard } from "./cellSlice";
+import { selectGame, setGameStats } from "./gameSlice";
 
 import { createBoard, nearbyCells } from "./boardUtils";
 
 import Cell from "./Cell";
 
 const Board = () => {
-  let boardSize = 9;
+  let boardSize = 8;
   let numOfMines = 10;
   let board = createBoard(boardSize, numOfMines);
   var boardStyle = { "--size": boardSize } as React.CSSProperties;
   const dispatch = useAppDispatch();
 
+  dispatch(setGameStats({ mineCount: numOfMines, size: boardSize }));
   dispatch(setBoard(board));
 
   return (
